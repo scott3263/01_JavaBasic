6cfa505275db86cd2a269104f12c921c502df1d7
scott3263 <scott00@naver.com>
2022-02-20 15:25:33 +0900

    테스트

--- src/kr/co/softcampus/beans/HelloWorldKo.java
+++ src/kr/co/softcampus/beans/HelloWorldKo.java
@@ -2,6 +2,6 @@ package kr.co.softcampus.beans;
 
 public class HelloWorldKo {
 	public void sayHello() {
-		System.out.println("HelloWorldKo 입니다..");
+		System.out.println("HelloWorldKo 입니다.....");
 	}
 }
